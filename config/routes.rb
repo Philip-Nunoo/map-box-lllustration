c9ab8ed1796c6ea8403b401766b466f090d47c27
ladmin <ladmin@iMac-02.local>
2014-07-14 10:46:46 +0000

    First scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Newapp::Application.routes.draw do
   get "home/index"
   get "home/about"
+  get "home/map"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
